5a8e412b97df7b4409e6fcea5fa85ee6b895b759
RbBtSn0w <hamiltonsnow@gmail.com>
2016-08-25 17:24:14 +0800

    Add file resources

---
+++ MoabCache.h
@@ -0,0 +1,158 @@
+//
+//  MoabCache.h
+//
+// Copyright (c) <2014-2016> Rbbtsn0w
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef enum {
+    MoabCacheErrorDirectoryIsFile = 1001
+} MoabCacheError;
+
+extern NSString * MoabCacheErrorDomain;
+
+@interface MoabCache : NSObject
+
+/*!
+ 
+ Designated initializer. You can custom the file document path.
+ 
+ @param name      Used to name internal NSCache instance and to name a folder which is used to store cached values persistently.
+ @param error     Will be assigned if something went wrong during initializing.
+ @param directory Will create document by NSSearchPathDirectory.
+ 
+ @return An instance of MoabCache
+ 
+ */
+- (nullable instancetype)initWithName:(NSString *)name error:(NSError *__autoreleasing *)error searchPathDirectory:(NSSearchPathDirectory) directory;
+
+/**
+ *
+ *  Designated initializer. Default document by NSCachesDirectory.
+ *
+ *  @param name  Used to name internal NSCache instance and to name a folder which is used to store cached values persistently.
+ *  @param error Will be assigned if something went wrong during initializing.
+ *
+ *  @return An instance of MoabCache
+ */
+- (nullable instancetype)initWithName:(NSString *)name error:(NSError *__autoreleasing *)error;
+
+/**
+ *
+ *  Instance's name.
+ *
+ *  @return cache name
+ */
+- (NSString *)name;
+
+/**
+ *
+ *  Caches the object.
+ *
+ *  @param object Object which is to be cached. It should conform to NSCoding protocol. Passing nil will delete corresponding object.
+ *  @param key    the key which should be used for the object above.
+ */
+- (void)setObject:(id<NSCoding>)object forKey:(NSString *)key;
+
+/**
+ *
+ *  Indicates whether object exists for the key or not.
+ *
+ *  @param key The key of the object.
+ *
+ *  @return Boolean indicating if an object is stored in the cache.
+ */
+- (BOOL)objectExistsForKey:(NSString *)key;
+
+
+/**
+ *
+ *  Returns an object for the corresponding key.
+ *
+ *  @param key The key of the object.
+ *
+ *  @return An object or nil if the object is not in the cache
+ */
+- (nullable id)objectForKey:(NSString *)key;
+
+/**
+ *
+ *  Removes an object from the cache.
+ *
+ *  @param key Object's key
+ */
+- (void)removeObjectForKey:(NSString *)key;
+
+/**
+ *
+ *  Removes all objects from the cache and persistent store.
+ */
+- (void)removeAllObjects;
+
+/**
+ *
+ *  Removes all objects from in-memory cache.
+ */
+- (void)clearMemory;
+
+/* Subscripting support */
+- (void)setObject:(id)obj forKeyedSubscript:(NSString *)key;
+- (nullable id)objectForKeyedSubscript:(NSString *)key;
+
+/**
+ *
+ *  Returnes object's file path.
+ *
+ *  @param key The key of the object.
+ *
+ *  @return Path if object exists, nil otherwise.
+ *  @exception MoabCacheKeyIsNilException will be raised if the key provided is nil.
+ */
+- (nullable NSString *)pathForObjectForKey:(NSString *)key;
+
+
+/**
+ *
+ *  Removes all objects from names, Only by NSCachesDirectory document.
+ */
++ (void)removeAllNameCache;
+
+
+/**
+ *
+ *  Removes all objects from names, Only by NSCachesDirectory document.
+ *
+ *  @param directory    Removes by NSSearchPathDirectory document.
+ */
++ (void)removeAllNameCacheBySearchPathDirectory:(NSSearchPathDirectory) directory;
+
+/**
+ *
+ *  Statistics cache folder size
+ *
+ *  @return size is long long
+ */
++ (long long)statisticsCacheFolderSize;
+
+@end
+NS_ASSUME_NONNULL_END
---
+++ MoabCache.m
@@ -0,0 +1,439 @@
+//
+//  MoabCache.m
+//
+// Copyright (c) <2014-2016> Rbbtsn0w
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "MoabCache.h"
+#import <sys/xattr.h>
+#import <sys/stat.h>
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+static NSString *const kMoabCacheErrorDomain = @"com.Rbbtsn0w.MoabCache.cache";
+
+static NSString *const kCacheQueueName = @"com.Rbbtsn0w.MoabCache.cache";
+
+static NSString *const kAllNameKeys = @"kMoabCacheNameKeys";
+
+
+
+static inline NSString *URLEncodeString(NSString *string);
+
+static inline long long fileSizeAtPath(NSString *filePath);
+
+NSString* AllNameKeysBySearchPathDirectory(NSSearchPathDirectory directory);
+
+@implementation MoabCache
+{
+    NSCache *_cache;
+    NSFileManager *_fileManager;
+    NSString *_cachesPath;
+    dispatch_queue_t _queue;
+}
+
+- (void)dealloc
+{
+    dispatch_barrier_sync(_queue, ^{}); //wait till the queue will finish all tasks
+#if !__has_feature(objc_arc)
+    [_cache release];
+    [_fileManager release];
+    [_cachesPath release];
+    
+    dispatch_release(_queue);
+    [super dealloc];
+#endif
+}
+
+#pragma mark    - Helper Methods
+
+- (NSString *)desiredPathForObjectForKey:(NSString *)key
+{
+    return [_cachesPath stringByAppendingPathComponent:URLEncodeString(key)];
+}
+
+- (void)saveAndCheckRepeatName:(NSString*)name withSearchPathDirectory:(NSSearchPathDirectory) directory
+{
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *allNameKeys = AllNameKeysBySearchPathDirectory(directory);
+    NSArray *finaArray = [userDefaults arrayForKey:allNameKeys];
+    NSMutableArray *mValues = nil;
+    
+    if (finaArray){
+        mValues = [finaArray mutableCopy];
+    }else{
+        mValues = [[NSMutableArray alloc] init];
+    }
+    
+    NSInteger indx = [mValues indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        return [obj isEqualToString:name];
+    }];
+    if (indx == NSNotFound) {
+        [mValues addObject:name];
+        [userDefaults setObject:mValues forKey:allNameKeys];
+    }
+    
+#if !__has_feature(objc_arc)
+    [mValues release];
+#endif
+    
+}
+
+- (void)removeAllObjectsBySync
+{
+    dispatch_barrier_sync(_queue, ^{
+        [_cache removeAllObjects];
+        NSArray *files = [_fileManager contentsOfDirectoryAtPath:_cachesPath error:NULL];
+        for (NSString *file in files) {
+            [_fileManager removeItemAtPath:[_cachesPath stringByAppendingPathComponent:file] error:NULL];
+        }
+    });
+}
+
+#pragma mark    - Interface Methods
+- (nullable instancetype)initWithName:(NSString *)name error:(NSError *__autoreleasing *)e searchPathDirectory:(NSSearchPathDirectory) directory {
+    self = [super init];
+    
+    __autoreleasing NSError *error = nil;
+    if (self) {
+        NSString *channelCacheQueue = [NSString stringWithFormat:@"%@_%@",kCacheQueueName, name];
+        _queue = dispatch_queue_create([channelCacheQueue UTF8String], DISPATCH_QUEUE_CONCURRENT);
+        _fileManager = [[NSFileManager alloc] init];
+        _cache = [[NSCache alloc] init];
+        
+        [_cache setName:name];
+        
+        NSString *userCaches = [NSSearchPathForDirectoriesInDomains(directory, NSUserDomainMask, YES) lastObject];
+        _cachesPath = [userCaches stringByAppendingPathComponent:URLEncodeString(name)];
+#if !__has_feature(objc_arc)
+        [_cachesPath retain];
+#endif
+        BOOL isDirectory = NO;
+        if (![_fileManager fileExistsAtPath:_cachesPath isDirectory:&isDirectory]) {
+            [_fileManager createDirectoryAtPath:_cachesPath withIntermediateDirectories:YES attributes:nil error:&error];
+        }
+        else if (!isDirectory) {
+            error = [NSError errorWithDomain:kMoabCacheErrorDomain code:MoabCacheErrorDirectoryIsFile userInfo:@{}];
+        }
+        
+        if (error) {
+            if (e) {
+                *e = error;
+            }
+#if !__has_feature(objc_arc)
+            [self release];
+#endif
+            self = nil;
+        }else{
+            
+            [self saveAndCheckRepeatName:name withSearchPathDirectory:directory];
+        }
+    }
+    
+    return self;
+}
+
+- (nullable instancetype)initWithName:(NSString *)name error:(NSError *__autoreleasing *)error
+{
+    return [self initWithName:name error:error searchPathDirectory:NSCachesDirectory];
+}
+
+- (NSString *)name
+{
+    return [_cache name];
+}
+
+- (void)setObject:(id<NSCoding>)object forKey:(NSString *)key
+{
+    if (key != nil) {
+        if (!object && [self objectExistsForKey:key]) {
+            [self removeObjectForKey:key];
+        }
+        else {
+            if ([(id)object conformsToProtocol:@protocol(NSCoding)]) {
+                dispatch_barrier_async(_queue, ^{
+                    @try {
+                        [_cache setObject:object forKey:key];
+                        [NSKeyedArchiver archiveRootObject:object toFile:[self desiredPathForObjectForKey:key]];
+                    }
+                    @catch (NSException *exception) {
+                        NSLog(@"Exception %@",exception);
+                        [self removeAllObjects];
+                    }
+                    @catch (...){
+                        NSLog(@"Exception catch ...");
+                        [self removeAllObjects];
+                    }
+                });
+            }
+        }
+    }
+}
+
+- (BOOL)objectExistsForKey:(NSString *)key
+{
+    __block BOOL objectExists = NO;
+    if (key != nil) {
+        dispatch_sync(_queue, ^{
+            objectExists = [_cache objectForKey:key] != nil;
+            if (!objectExists) {
+                objectExists = [_fileManager fileExistsAtPath:[self desiredPathForObjectForKey:key]];
+            }
+        });
+    }
+    return objectExists;
+}
+
+- (nullable id)objectForKey:(NSString *)key
+{
+    __block id object = nil;
+    if (key != nil) {
+        dispatch_sync(_queue, ^{
+            object = [_cache objectForKey:key];
+#if !__has_feature(objc_arc)
+            [object retain];
+#endif
+            if (!object) {
+                @try {
+                    object = [NSKeyedUnarchiver unarchiveObjectWithFile:[self desiredPathForObjectForKey:key]];
+                }
+                @catch (NSException *exception) {
+                    NSLog(@"Exception %@",exception);
+                    object = nil;
+                    [self removeAllObjects];
+                }
+                @catch (...){
+                    NSLog(@"Exception catch ...");
+                    object = nil;
+                    [self removeAllObjects];
+                }
+                
+                if (object != nil) {
+#if !__has_feature(objc_arc)
+                    [object retain]; //this one is for autorelease before return
+                    [object retain]; //this one is for autorelease in barrier's block
+#endif
+                    dispatch_barrier_async(_queue, ^{
+                        [_cache setObject:object forKey:key];
+#if !__has_feature(objc_arc)
+                        [object release];
+#endif
+                    });
+                }
+            }
+        });
+    }
+#if !__has_feature(objc_arc)
+    [object autorelease];
+#endif
+    return object;
+}
+
+- (void)removeObjectForKey:(NSString *)key
+{
+    if (key != nil) {
+        dispatch_barrier_async(_queue, ^{
+            [_cache removeObjectForKey:key];
+            [_fileManager removeItemAtPath:[self desiredPathForObjectForKey:key] error:NULL];
+        });
+    }
+}
+
+- (void)removeAllObjects
+{
+    dispatch_barrier_async(_queue, ^{
+        [_cache removeAllObjects];
+        NSArray *files = [_fileManager contentsOfDirectoryAtPath:_cachesPath error:NULL];
+        for (NSString *file in files) {
+            [_fileManager removeItemAtPath:[_cachesPath stringByAppendingPathComponent:file] error:NULL];
+        }
+    });
+}
+
+- (void)clearMemory
+{
+    dispatch_barrier_async(_queue, ^{
+        [_cache removeAllObjects];
+    });
+}
+
+- (void)setObject:(id)obj forKeyedSubscript:(NSString *)key
+{
+    [self setObject:obj forKey:key];
+}
+
+- (nullable id)objectForKeyedSubscript:(NSString *)key
+{
+    return [self objectForKey:key];
+}
+
+- (nullable NSString *)pathForObjectForKey:(NSString *)key
+{
+    return [self objectExistsForKey:key] ? [self desiredPathForObjectForKey:key] : nil;
+}
+
++ (void) removeAllNameCache
+{
+    @try {
+        
+        NSUserDefaults *userdefault = [NSUserDefaults standardUserDefaults];
+        
+        NSArray *allChannels = [userdefault arrayForKey:kAllNameKeys];
+        
+        if (allChannels && [allChannels count] > 0){
+            
+            for (NSString *channelID in allChannels) {
+                if ([channelID isEqualToString:@"get_detail_info"]) {
+                    continue;
+                }
+                NSError *error = nil;
+                MoabCache *moabCache = [[MoabCache alloc] initWithName:channelID error:&error];
+                [moabCache removeAllObjectsBySync];
+                if (error) {
+                    NSLog(@"%@",[NSString stringWithFormat:@"Remove channel %@ cache was failure",channelID]);
+                }
+#if !__has_feature(objc_arc)
+                [moabCache release];
+#endif
+            }
+        }
+        
+        [userdefault removeObjectForKey:kAllNameKeys];
+        [userdefault synchronize];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Remove all channel's cache was failure!, Exception:%@",exception);
+    }
+}
+
++ (void)removeAllNameCacheBySearchPathDirectory:(NSSearchPathDirectory) directory{
+    @try {
+        
+        NSUserDefaults *userdefault = [NSUserDefaults standardUserDefaults];
+        
+        NSString *allNameKeys = AllNameKeysBySearchPathDirectory(directory);
+        NSArray *allChannels = [userdefault arrayForKey:allNameKeys];
+        
+        if (allChannels && [allChannels count] > 0){
+            
+            for (NSString *channelID in allChannels) {
+                if ([channelID isEqualToString:@"get_detail_info"]) {
+                    continue;
+                }
+                NSError *error = nil;
+                MoabCache *moabCache = [[MoabCache alloc] initWithName:channelID error:&error searchPathDirectory:directory];
+                [moabCache removeAllObjectsBySync];
+                if (error) {
+                    NSLog(@"%@",[NSString stringWithFormat:@"Remove channel %@ cache was failure",channelID]);
+                }
+#if !__has_feature(objc_arc)
+                [moabCache release];
+#endif
+            }
+        }
+        
+        [userdefault removeObjectForKey:allNameKeys];
+        [userdefault synchronize];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Remove all channel's cache was failure!, Exception:%@",exception);
+    }
+}
+
++ (long long) statisticsCacheFolderSize
+{
+    @try {
+        long long filesz = 0;
+        
+        NSUserDefaults *userdefault = [NSUserDefaults standardUserDefaults];
+        
+        NSArray *allChannels = [userdefault arrayForKey:kAllNameKeys];
+        
+        if (allChannels && [allChannels count] > 0){
+            
+            for (NSString *channelID in allChannels) {
+                NSError *error = nil;
+                MoabCache *moabCache = [[MoabCache alloc] initWithName:channelID error:&error];
+                
+                NSString *cachePath = moabCache->_cachesPath;
+                
+                filesz += fileSizeAtPath(cachePath);
+                
+                if (error) {
+                    NSLog(@"%@",[NSString stringWithFormat:@"Remove channel %@ cache was failure",channelID]);
+                }
+#if !__has_feature(objc_arc)
+                [moabCache release];
+#endif
+            }
+        }
+        
+        return filesz;
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Remove all channel's cache was failure!, Exception:%@",exception);
+    }
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
+static inline NSString *URLEncodeString(NSString *string)
+{
+#if __has_feature(objc_arc)
+    return (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(NULL,
+                                                                                 (__bridge CFStringRef)string,
+                                                                                 NULL,
+                                                                                 (__bridge CFStringRef)@"!*'\"();:@&=+$,/?%#[]% ",
+                                                                                 kCFStringEncodingUTF8
+                                                                                 );
+#else
+    return [(NSString *)CFURLCreateStringByAddingPercentEscapes(NULL,
+                                                                (CFStringRef)string,
+                                                                NULL,
+                                                                (CFStringRef)@"!*'\"();:@&=+$,/?%#[]% ",
+                                                                kCFStringEncodingUTF8
+                                                                ) autorelease];
+#endif
+}
+
+
+static inline long long fileSizeAtPath(NSString *filePath)
+{
+    struct stat st;
+    if (lstat([filePath cStringUsingEncoding:NSUTF8StringEncoding], &st) == 0) {
+        return st.st_size;
+    }
+    return 0;
+}
+
+NSString* AllNameKeysBySearchPathDirectory(NSSearchPathDirectory directory){
+    
+    NSString *name = nil;
+    if (directory == NSCachesDirectory) {
+        name = kAllNameKeys;
+    }else{
+        name = [NSString stringWithFormat:@"%@_%tu", kAllNameKeys, directory];
+    }
+    
+    return name;
+}
